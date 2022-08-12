58c3aa67d996465db746efd53099ac9ec8739b04
parrgraham <94260748+parrgraham@users.noreply.github.com>
2022-08-12 18:59:22 +1200

    Update MPU6050_A.h

--- MPU6050_A.h
+++ MPU6050_A.h
@@ -1,17 +1,23 @@
-#ifdef i2c_default
+//i2c_default gets defined in the i2c.h file ,need to put more hours into that part
 
+#ifdef i2c_default
+//as per the datasheet and register map
+//https://invensense.tdk.com/wp-content/uploads/2015/02/MPU-6000-Datasheet1.pdf
+//https://invensense.tdk.com/wp-content/uploads/2015/02/MPU-6000-Register-Map1.pdf
 static int addr = 0x68;
 static void mpu6050_reset() {
     uint8_t buf[] = {0x6B, 0x00};
     i2c_write_blocking(i2c_default, addr, buf, 2, false);
 }
-
+//def need to anki up some of the basic commands
+//also data types and binary table and pointers
+//also binary math tricks
 static void mpu6050_read_raw(int16_t accel[3], int16_t gyro[3], int16_t *temp) {
 
     uint8_t buffer[6];
-
     uint8_t val = 0x3B;
-    i2c_write_blocking(i2c_default, addr, &val, 1, true); // true to keep master control of bus
+    
+    i2c_write_blocking(i2c_default, addr, &val, 1, true); 
     i2c_read_blocking(i2c_default, addr, buffer, 6, false);
 
     for (int i = 0; i < 3; i++) {
